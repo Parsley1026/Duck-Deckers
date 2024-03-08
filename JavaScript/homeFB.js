e4b6148e65f0af0386de436a852187bb3df480c7
ethanp718 <56520893+ethanp718@users.noreply.github.com>
2024-03-08 11:58:47 -0600

    Update homeFB.js

--- JavaScript/homeFB.js
+++ JavaScript/homeFB.js
@@ -97,7 +97,7 @@ let joinRoom = evt => {
 }
 
 //create my signOut method
-let signOut = ()=>{
+let signOut = () =>{
     if(confirm("Are you sure you want to sign out?") == true) {//make sure user wants to sign out
         auth.signOut(); //sign out from the database
         window.location.href = 'home.html'; //switch the window from home to login
